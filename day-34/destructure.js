09552f5ad4f333eb963d1e4011411b949c9c461a
harsh-100 <26harshagarwal@gmail.com>
2023-10-12 20:21:40 +0530

    adding the 34th day

---
+++ day-34/destructure.js
@@ -0,0 +1,25 @@
+const std = {
+  name: "harsh",
+  age: 23,
+  race: "human",
+};
+let name = "checkc";
+
+let { name: Fname } = std;
+console.log(newName);
+
+let arr = ["asdfsf", "agaerak", "third", "fourth"];
+// console.log(arr[2])
+let [fName, , , tName] = arr;
+console.log(fName);
+console.log(tName);
+
+const members = [
+  { name: "Vijay Gupta", age: 20 },
+  { name: "Yash Jangid", age: 40 },
+  { name: "Firoz Khan", age: 41 },
+  { name: "Amrit Srivastava", age: 17 },
+  { name: "Chandraprakash Sharma" },
+  { name: "Swpril Ahuja", age: 45 },
+  { name: "Yogesh Khatri", age: 51 },
+];
